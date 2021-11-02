d46e4dfee2c8370fca910501b1a3b853bb0ec4ef
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-01 21:19:07 -0300

    change number

--- fundamentos/firstComand.js
+++ fundamentos/firstComand.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+let a = 7;
+let b = 94;
+
+// trocando o valor das variaveis
+let temp = a;
+a = b;
+b = temp;
+
+// outra forma
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
